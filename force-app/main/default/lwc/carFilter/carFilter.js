d86ae68e03f418e679b59736917344bb39c3b9f0
BabarKhann <babarkhan138@gmail.com>
2023-12-06 20:59:51 +0500

    Design carFilter component

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,3 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
 
-export default class CarFilter extends LightningElement {}
+import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
+import Car_Object from "@salesforce/schema/Car__c";
+import CATEGORY from "@salesforce/schema/Car__c.Category__c";
+import MAKE from "@salesforce/schema/Car__c.Make__c";
+
+export default class CarFilter extends LightningElement {
+  filters = {
+    searchKey: "",
+    price: 999999
+  };
+
+  @wire(getObjectInfo, { objectApiName: Car_Object })
+  carObjectInfo;
+
+  @wire(getPicklistValues, {
+    recordTypeId: "$carObjectInfo.data.defaultRecordTypeId",
+    fieldApiName: CATEGORY
+  })
+  categories;
+
+  @wire(getPicklistValues, {
+    recordTypeId: "$carObjectInfo.data.defaultRecordTypeId",
+    fieldApiName: MAKE
+  })
+  maketype;
+
+  handleSearchChange(e) {
+    this.filters = { ...this.filters, searchKey: e.target.value };
+
+    console.log(JSON.stringify(this.filters));
+  }
+
+  handlePriceChange(e) {
+    this.filters = { ...this.filters, price: e.target.value };
+
+    console.log(JSON.stringify(this.filters));
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target.dataset;
+    console.log("name", name);
+    console.log("value", value);
+  }
+}
