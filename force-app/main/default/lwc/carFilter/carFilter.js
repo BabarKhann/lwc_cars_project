eb2ac19e08055ab382adc949bf5cda93dccc4737
BabarKhann <babarkhan138@gmail.com>
2023-12-20 23:35:51 +0500

    Getting data on based on filters with LMS

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -5,11 +5,21 @@ import Car_Object from "@salesforce/schema/Car__c";
 import CATEGORY from "@salesforce/schema/Car__c.Category__c";
 import MAKE from "@salesforce/schema/Car__c.Make__c";
 
+//LMS
+import { publish, MessageContext } from "lightning/messageService";
+// MessageChannel
+import CarsFilterMessageChannel from "@salesforce/messageChannel/CarsFilters__c";
+
 export default class CarFilter extends LightningElement {
   filters = {
     searchKey: "",
     price: 999999
   };
+
+  timer;
+
+  @wire(MessageContext)
+  messageContext;
 
   @wire(getObjectInfo, { objectApiName: Car_Object })
   carObjectInfo;
@@ -28,19 +38,46 @@ export default class CarFilter extends LightningElement {
 
   handleSearchChange(e) {
     this.filters = { ...this.filters, searchKey: e.target.value };
-
     console.log(JSON.stringify(this.filters));
+    this.sendDataToCarList();
   }
 
   handlePriceChange(e) {
     this.filters = { ...this.filters, price: e.target.value };
-
     console.log(JSON.stringify(this.filters));
+    this.sendDataToCarList();
   }
 
   handleChange(e) {
+    if (!this.filters.categories) {
+      const categories = this.categories.data.values.map(
+        (value) => value.value
+      );
+      const maketype = this.maketype.data.values.map((value) => value.value);
+
+      this.filters = { ...this.filters, categories, maketype };
+    }
     const { name, value } = e.target.dataset;
     console.log("name", name);
     console.log("value", value);
+    if (e.target.checked) {
+      if (!this.filters[name].includes(value)) {
+        this.filters[name] = [...this.filters[name], value];
+      }
+    } else {
+      this.filters[name] = this.filters[name].filter((item) => item !== value);
+    }
+
+    this.sendDataToCarList();
+  }
+
+  sendDataToCarList() {
+    clearTimeout(this.timer);
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.timer = setTimeout(() => {
+      publish(this.messageContext, CarsFilterMessageChannel, {
+        filters: this.filters
+      });
+    }, 400);
   }
 }
--- force-app/main/default/lwc/carList/carList.js
+++ force-app/main/default/lwc/carList/carList.js
@@ -2,19 +2,50 @@ import { LightningElement, wire } from "lwc";
 
 import getCars from "@salesforce/apex/CarController.getCars";
 
+//LMS
+import { subscribe, MessageContext } from "lightning/messageService";
+import CarsFilterMessageChannel from "@salesforce/messageChannel/CarsFilters__c";
+
 export default class CarList extends LightningElement {
   cars;
   error;
 
   filters = {};
+  subscription;
+
+  @wire(MessageContext)
+  messageContext;
 
   @wire(getCars, { filters: "$filters" })
   handleCars({ data, error }) {
     if (data) {
       this.cars = data;
+      console.log(JSON.stringify(data));
     }
     if (error) {
       this.error = error;
+      console.log(error);
     }
   }
+
+  subscribeHandler() {
+    this.subscription = subscribe(
+      this.messageContext,
+      CarsFilterMessageChannel,
+      (message) => {
+        this.getFilters(message);
+      }
+    );
+  }
+
+  getFilters(message) {
+    this.filters = { ...message.filters };
+    console.log(JSON.stringify(this.filters), "⬇️⬇️⬇️");
+  }
+
+  connectedCallback() {
+    this.subscribeHandler();
+  }
+
+  disconnectedCallback() {}
 }
